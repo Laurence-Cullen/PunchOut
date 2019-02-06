ac2a84e0760c24d378eafcf200eb795e164bf573
Greg Bland <gbland94@hotmail.com>
2019-02-06 23:13:33 +0000

    Adding .py keras to json model converter

--- keras_to_json_converter.py
+++ keras_to_json_converter.py
@@ -1,39 +1,18 @@
-from sklearn.datasets import load_files
-from keras.utils import np_utils
-import numpy as np
-from glob import glob
-from keras.preprocessing import image
-from tqdm import tqdm
-from PIL import ImageFile
-from sklearn.model_selection import train_test_split
-from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
-from keras.layers import Dropout, Flatten, Dense
-from keras.models import Sequential
-from keras.callbacks import ModelCheckpoint
-from keras.callbacks import EarlyStopping
-from keras import optimizers
-from keras.layers import BatchNormalization
-from keras.regularizers import l1, l2
-from keras.layers import GaussianNoise
-from sklearn.model_selection import train_test_split
-from keras.preprocessing.image import ImageDataGenerator
-from keras.applications.resnet50 import ResNet50
-from keras.applications.resnet50 import preprocess_input
 import keras
-from keras.models import Model
-from keras.applications import MobileNet
+import tensorflowjs as tfjs
 from keras.applications import MobileNetV2
-import time
+from keras.layers import Dense
+from keras.layers import GlobalAveragePooling2D
+from keras.models import Model
 
 base_model = MobileNetV2(weights='imagenet', include_top=False)
 
 base_model.summary()
 
-
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(1024, activation='relu')(x)
-predictions = Dense(3, activation='softmax')(x)
+predictions = Dense(4, activation='softmax')(x)
 # this is the model we will train
 mobilenetV2_model = Model(inputs=base_model.input, outputs=predictions)
 mobilenetV2_model.summary()
@@ -41,8 +20,5 @@ mobilenetV2_model.compile(loss='categorical_crossentropy', optimizer=keras.optim
 
 mobilenetV2_model.load_weights('usable_models/duck-iteration_98percenttest.hdf5')
 
-# Python
-
-import tensorflowjs as tfjs
-
-tfjs.converters.save_keras_model(mobilenetV2_model_duck-iteration_98percenttest, 'Javascript_models')
+s
+tfjs.converters.save_keras_model(mobilenetV2_model, 'Javascript_models/temp')
